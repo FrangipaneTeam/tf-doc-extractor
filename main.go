5e0809d516f57306f93539d0f5a61464b91de2db
Ere Mid <eremid@gmail.com>
2023-02-20 14:51:54 +0100

    feat: add version message and check

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
+	"strings"
 
 	"github.com/FrangipaneTeam/terraform-templates/pkg/file"
 	"github.com/rs/zerolog/log"
 
+	"github.com/tcnksm/go-latest"
+
 	"github.com/FrangipaneTeam/tf-doc-extractor/internal/example"
 )
+
+//go:embed version.txt
+var version string
 
 func main() {
 	fileName := flag.String("filename", "", "filename")
@@ -16,6 +23,9 @@ func main() {
 	fromResource := flag.Bool("resource", false, "from resource")
 	flag.Parse()
 	// log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	version = strings.Trim(version, "\n")
+	log.Info().Msgf("tf-doc-extractor version %s", version)
 
 	if *fileName == "" {
 		log.Fatal().Msg("filename is required")
@@ -31,6 +41,21 @@ func main() {
 
 	if *fromTest && *fromResource {
 		log.Fatal().Msg("test and resource are exclusive")
+	}
+
+	// check version
+	githubTag := &latest.GithubTag{
+		Owner:      "FrangipaneTeam",
+		Repository: "tf-doc-extractor",
+	}
+
+	res, err := latest.Check(githubTag, version)
+	if err == nil {
+		if res.Outdated {
+			log.Warn().Msgf("new version availaible : %s", res.Current)
+		}
+	} else {
+		log.Warn().Err(err).Msg("failed to check version")
 	}
 
 	log.Info().Msgf("using file %s", *fileName)
