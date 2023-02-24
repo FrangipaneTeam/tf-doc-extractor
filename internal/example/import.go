34ab32b771ec03d6c594596330071f7be7fba62c
Mickael Stanislas <contact@mickael-stanislas.com>
2023-02-24 17:14:12 +0100

    feat: add categoryName in import

--- internal/example/import.go
+++ internal/example/import.go
@@ -14,7 +14,7 @@ import (
 
 func genExampleFromResource(str string) (string, string) {
 	importNameRe := regexp.MustCompile(`resource\.ImportStatePassthroughID\(ctx, path\.Root\("(\S+)"\), req, resp\)`)
-	tfNameRe := regexp.MustCompile(`resp\.TypeName = req\.ProviderTypeName \+ "_(\S+)"`)
+	tfNameRe := regexp.MustCompile(`resp\.TypeName = req\.ProviderTypeName \+ "_" \+ categoryName \+ "_" \+ "(\S+)"`)
 
 	importName := ""
 	tfName := ""
@@ -39,6 +39,34 @@ func genExampleFromResource(str string) (string, string) {
 	return importName, tfName
 }
 
+func getCategoryFromBase(resourceFile string) string {
+
+	categoryNameRe := regexp.MustCompile(`categoryName = "(\S+)"`)
+	categoryName := ""
+
+	x := strings.Split(resourceFile, "/")
+	x[len(x)-1] = "base.go"
+	fileCategory := strings.Join(x, "/")
+
+	f, err := file.ToString(fileCategory)
+	if err != nil {
+		return ""
+	}
+
+	scannerCategory := bufio.NewScanner(strings.NewReader(f))
+	for scannerCategory.Scan() {
+		line := scannerCategory.Text()
+
+		if categoryNameRe.MatchString(line) {
+			logger.Logger.Info().Msgf("found category name: %s", categoryName)
+			categoryName = categoryNameRe.FindStringSubmatch(line)[1]
+			break
+		}
+	}
+
+	return categoryName
+}
+
 func CreateImportExampleFile(fileName, exampleDir string) error {
 	f, err := file.ToString(fileName)
 	if err != nil {
@@ -47,15 +75,23 @@ func CreateImportExampleFile(fileName, exampleDir string) error {
 
 	importName, tfName := genExampleFromResource(f)
 	logger.Logger.Info().Msgf("importName: %s", importName)
+	logger.Logger.Info().Msgf("tfName: %s", tfName)
 
-	exampleDirPath := exampleDir + "/resources/cloudavenue_" + tfName
+	categoryName := getCategoryFromBase(fileName)
+	logger.Logger.Info().Msgf("categoryName: %s", categoryName)
+
+	if importName == "" || tfName == "" || categoryName == "" {
+		return fmt.Errorf("failed to generate import example for %s", fileName)
+	}
+
+	exampleDirPath := exampleDir + "/resources/cloudavenue_" + categoryName + "_" + tfName
 	errMkdir := os.MkdirAll(exampleDirPath, 0o755)
 	if errMkdir != nil {
 		return errMkdir
 	}
 
 	doc := fmt.Sprintf("# use the %s to import the resource\n", importName)
-	doc += fmt.Sprintf("terraform import cloudavenue_%s.example %s", tfName, importName)
+	doc += fmt.Sprintf("terraform import cloudavenue_%s_%s.example %s", categoryName, tfName, importName)
 
 	errWrite := os.WriteFile(exampleDirPath+"/import.sh", []byte(doc), 0o644)
 	if errWrite != nil {
